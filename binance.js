827243a0e57a1cd2a04cbc4abfab875dc416de4d
Joao Valle <gustojvalle@outlook.com>
2021-05-05 17:14:47 +0100

    added percentage to the title

--- binance.js
+++ binance.js
@@ -3,6 +3,9 @@ const dogePriceFetching = () => {
     axios.request('https://api.coingecko.com/api/v3/coins/dogecoin').then(function (response) {
         const usdPrice = response.data.market_data.current_price.usd
         document.querySelector(".doge").innerText = `${usdPrice} USD`;
+        const percent = Math.floor((1-(1-Number(usdPrice)))*100);
+        document.querySelector(".dynamic-title").innerText = `DogeCoin To 1USD ${percent}%`
+        
     }).catch(function (error) {
         console.error(error);
     });}
